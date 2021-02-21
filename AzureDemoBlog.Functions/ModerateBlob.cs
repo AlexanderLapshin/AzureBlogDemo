140a0dc9ae4fe3152aaa02c265c149ee1d4be19d
Alexander Lapshin <alex2@lapshin.org.ua>
2021-02-21 20:33:29 +0200

    removed unnecessary usings

--- AzureDemoBlog.Functions/ModerateBlob.cs
+++ AzureDemoBlog.Functions/ModerateBlob.cs
@@ -1,8 +1,6 @@
-﻿using System;
-using System.IO;
+﻿using System.IO;
 using System.Threading.Tasks;
 using Microsoft.Azure.WebJobs;
-using Microsoft.Azure.WebJobs.Host;
 using Microsoft.Extensions.Logging;
 using Microsoft.WindowsAzure.Storage.Blob;
 using Newtonsoft.Json;
